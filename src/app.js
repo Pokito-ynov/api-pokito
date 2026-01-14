cdca07f708192a544c9380b8b3429cad0ceb0600
Nadjide <106732960+Nadjide@users.noreply.github.com>
2026-01-14 15:08:08 +0100

    feat: Initialize the Express application with core middleware, API routes for users, tables, and games, and a health check endpoint.

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors());
-app.use(express.json()); g
+app.use(express.json());
 
 app.use('/users', usersRoutes);
 app.use('/tables', tablesRoutes);
